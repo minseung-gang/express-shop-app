175c01a92453e219c1beb416cac53bcd55bd886c
minseung-gang <strause1@nate.com>
2025-01-28 19:54:44 +0900

    Feat:  상품 수정 및 추가, 삭제 라우터 추가

--- src/routes/admin-products.router.js
+++ src/routes/admin-products.router.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import { checkAdmin } from '../middleware/auth.js'
 import {
-  getCategories,
+  renderAddProductPage,
   postProduct,
   getProducts,
+  deleteProduct,
+  getProduct,
+  postThumbImage,
+  deleteThumbImage,
 } from '../controllers/product.controller.js'
 
 export const adminProductsRouter = express.Router()
 
-adminProductsRouter.get('/add-product', checkAdmin, getCategories)
+adminProductsRouter.get('/add-product', checkAdmin, renderAddProductPage)
 adminProductsRouter.get('/', checkAdmin, getProducts)
 adminProductsRouter.post('/', checkAdmin, postProduct)
+adminProductsRouter.delete('/:id', checkAdmin, deleteProduct)
+adminProductsRouter.get('/:id/edit', checkAdmin, getProduct)
+adminProductsRouter.post('/product-gallery/:id', checkAdmin, postThumbImage)
+adminProductsRouter.delete('/:id/image/:imageId', checkAdmin, deleteThumbImage)
