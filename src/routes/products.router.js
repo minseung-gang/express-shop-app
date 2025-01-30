0b3cf39ee204a303d92ccec05c43f8feeb4fd8ae
minseung-gang <strause1@nate.com>
2025-01-30 22:13:08 +0900

    Feat: 상품 관련 라우트 추가 및 컨트롤러 연결

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import { getAllCategories } from '../middleware/product.js'
+import {
+  renderProductPage,
+  renderCategoryPage,
+  renderProductsPage,
+  addProductToCart,
+} from '../controllers/product.controller.js'
 
 export const productsRouter = express.Router()
 
-productsRouter.use('/', (req, res) => {
-  res.render('products')
-})
+productsRouter.get('/', getAllCategories, renderProductsPage)
+productsRouter.get('/:category', getAllCategories, renderCategoryPage)
+productsRouter.get('/:category/:product', renderProductPage)
+productsRouter.post('/:productId', addProductToCart)
