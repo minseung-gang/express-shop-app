6209294b89f8b9a851e94dbf5360b1dc38c5b395
minseung-gang <strause1@nate.com>
2025-01-30 22:09:11 +0900

    Refector:  관리자 컨트롤러를 분리하여 코드 구조 개선

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,22 +3,10 @@ import { Category } from '../models/categories.model.js'
 import { Product } from '../models/products.model.js'
 import fs from 'fs-extra'
 
-export async function renderAddProductPage(req, res, next) {
-  try {
-    const categories = await Category.find()
-
-    res.render('admin/add-product', {
-      categories,
-    })
-  } catch (err) {
-    next(err)
-  }
-}
-
-export async function getProducts(req, res, next) {
+export async function renderProductsPage(req, res, next) {
   try {
     const products = await Product.find()
-    res.render('admin/products', {
+    res.render('products', {
       products,
     })
   } catch (err) {
@@ -26,122 +14,82 @@ export async function getProducts(req, res, next) {
   }
 }
 
-export async function postProduct(req, res, next) {
-  const imageFile =
-    typeof req.files?.image !== 'undefined' ? req.files.image.name : ''
-  const { title, desc, price, category } = req.body
-  const slug = imageFile.replace(/\s+/g, '-').toLowerCase()
-
-  // 데이터 베이스에 저장
-  // 새로운 인스턴스를 만드는 이유 => 객체의 데이터를 수정한 후 저장.
-  const newProduct = new Product({
-    title,
-    desc,
-    price,
-    category,
-    slug,
-    image: imageFile,
-  })
+export async function renderCategoryPage(req, res, next) {
+  const categorySlug = req.params.category
   try {
-    await newProduct.save()
-
-    await fs.mkdir('src/public/product-images/' + newProduct._id, {
-      recursive: true,
+    const products = await Product.find({ category: categorySlug })
+    res.render('products', {
+      products,
     })
-    await fs.mkdir('src/public/product-images/' + newProduct._id + '/gallery')
-    await fs.mkdir(
-      'src/public/product-images/' + newProduct._id + '/gallery/thumbs'
-    )
-
-    // 이미지 파일을 폴더에 넣어주기
-    const path = 'src/public/product-images/' + newProduct._id + '/' + imageFile
-    const productImage = req.files.image
-    await productImage.mv(path)
-
-    req.flash('success', '상품이 추가되었습니다.')
-    res.redirect('/admin/products')
   } catch (err) {
     next(err)
   }
 }
 
-export async function deleteProduct(req, res, next) {
-  const id = req.params.id
-  const path = 'src/public/product-images/' + id
+export async function renderProductPage(req, res, next) {
   try {
-    await Product.findByIdAndDelete(id)
-    await fs.remove(path)
-    req.flash('success', '상품이 삭제되었습니다.')
-    res.redirect('back')
-  } catch (err) {
-    next(err)
-  }
-}
+    const product = await Product.findOne({ slug: req.params.product })
 
-export async function getProduct(req, res, next) {
-  try {
-    const categories = await Category.find()
-    const { _id, title, desc, slug, category, price, image } =
-      await Product.findById(req.params.id)
-    const galleryDir = 'src/public/product-images/' + _id + '/gallery/thumbs'
+    if (!product) {
+      return res.status(404).json({ message: '상품을 찾을 수 없습니다.' })
+    }
+
+    const galleryDir = 'src/public/product-images/' + product._id + '/gallery'
     const galleryImages = await fs.readdir(galleryDir)
 
-    return res.render('admin/edit-product', {
-      id: _id,
-      title,
-      desc,
-      slug,
-      categories,
-      category: category.replace(/\s+/g, '-').toLowerCase(),
-      price,
+    res.render('product', {
+      product,
       galleryImages,
-      image,
     })
   } catch (err) {
     next(err)
   }
 }
 
-export async function postThumbImage(req, res, next) {
-  const productImage = req.files.file
-  const id = req.params.id
-  const path =
-    'src/public/product-images/' + id + '/gallery/' + req.files.file.name
-  const thumbsPath =
-    'src/public/product-images/' + id + '/gallery/thumbs/' + req.files.file.name
-
+export async function addProductToCart(req, res, next) {
+  const slug = req.params.product
   try {
-    // 원본 이미지 폴더에 넣어주기
-    await productImage.mv(path)
+    // 상품의 아이디를 가지고 어디에 추가해야함
+    // 어디에 추가해야할까가 관건
+    // 장바구니라는 임시 저장장소가 필요한걸까?
+    // 그걸 api를 통해서 누를때마다 저장하고 다시 받아오고 그런건가?
+    // 나는 로컬스토리지나 세션스토리지에 저장하고 사용하는 줄 알았는데 그게아닌것같다.
+    const product = await Product.findOne({ slug })
 
-    const buf = await ResizeImg(fs.readFileSync(path), {
-      width: 100,
-      height: 100,
-    })
+    // 처음 카트에 상품을 넣을떄
+    if (!req.session.cart) {
+      req.session.cart = []
+      req.session.cart.push({
+        title: slug,
+        qty: 1,
+        porice: product.price,
+        image: '/product-images/' + product._id + '/' + product.image,
+      })
+    } else {
+      let cart = req.session.cart
+      let newItem = true
 
-    fs.writeFileSync(thumbsPath, buf)
+      // 이미 카트에 있는 상품일 때 => 한개 추가하고 loop break
+      for (let i = 0; i < cart.length; i++) {
+        if (cart[i].slug === slug) {
+          cart[i].qty++
+          newItem = false
+          break
+        }
+      }
 
-    res.sendStatus(200)
-  } catch (err) {
-    next(err)
-  }
-}
-
-export async function deleteThumbImage(req, res, next) {
-  const id = req.params.id
-  const imageId = req.params.imageId
-  const originalImage =
-    'src/public/product-images/' + id + '/gallery/' + imageId
-  const thumbImage =
-    'src/public/product-images/' + id + '/gallery/thumbs/' + imageId
-  console.log('오리지날 이미지경로', originalImage)
-  console.log('썸네일 이미지 경로', thumbImage)
-
-  try {
-    await fs.remove(originalImage)
-    await fs.remove(thumbImage)
-    req.flash('success', '이미지가 삭제되었습니다.')
-    res.redirect('/admin/products/' + id + '/edit')
+      // 처음 추가하는 상품일 때
+      if (newItem) {
+        cart.push({
+          title: slug,
+          qty: 1,
+          price: product.price,
+          image: '/product-images' + product._id + '/' + product.image,
+        })
+      }
+      req.flash('success', '상품이 추가되었습니다.')
+      res.redirect('back')
+    }
   } catch (err) {
     next(err)
   }
