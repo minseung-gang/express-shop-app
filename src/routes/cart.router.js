088bdf27bcc2ab2cab5ab60c0a37b92c4a4d7fdf
minseung-gang <strause1@nate.com>
2025-02-01 00:45:12 +0900

    Feat: 결제모듈 성공 후 리다이렉트 할 라우트 추가

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -4,11 +4,13 @@ import {
   renderCartPage,
   updateCarteItem,
   deleteCartItem,
+  clearCartAfterPayment,
 } from '../controllers/cart.controller.js'
 
 export const cartRouter = express.Router()
 
 cartRouter.post('/:product', addProductToCart)
 cartRouter.get('/checkout', renderCartPage)
+cartRouter.get('/checkout/complete', clearCartAfterPayment)
 cartRouter.get('/update/:product', updateCarteItem)
 cartRouter.delete('/', deleteCartItem)
